84943e5b030e97f2787e3330e7155a6ac411c1af
Laszlo Treszkai <laszlo.treszkai@abacus.ac>
2021-06-13 21:20:34 +0200

    reformat setup.py according to packaging tutorial
    https://packaging.python.org/tutorials/packaging-projects/

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-setup(name='best',
-      description='Bayesian estimation supersedes the t-test',
-      author='Andrew Straw and Laszlo Treszkai',
-      author_email='laszlo.treszkai@gmail.com',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='https://github.com/treszkai/best',
-      version='2.0.1',
-      packages=['best'],
-      classifiers=[
-              'Programming Language :: Python :: 3',
-              'License :: OSI Approved :: MIT License',
-              'Operating System :: OS Independent',
-      ],
-      license='MIT',
-      python_requires='>=3.5',
-      install_requires=[
-          'numpy',
-          'scipy',
-          'matplotlib>=3.0.0',
-          'pymc3<4.0',
-      ],
-      )
+setup(
+    name='best',
+    version='2.0.1',
+    description='Bayesian estimation supersedes the t-test',
+    author='Andrew Straw and Laszlo Treszkai',
+    author_email='laszlo.treszkai@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/treszkai/best',
+    package_dir={'': 'best'},
+    packages=find_packages(where='src'),
+    classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+    ],
+    license='MIT',
+    python_requires='>=3.5',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib>=3.0.0',
+        'pymc3<4.0',
+    ],
+)
